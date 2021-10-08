77aa47621fbd75665a56fa3d2c26bf564b1183e6
Gohan <sarnam164@gmail.com>
2021-10-08 20:22:34 +0530

    added loading animation on login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
     password:''
   }
   hide = true;
+  loading = false;
 
   constructor(private loginservice:LoginService, private snackbar:MatSnackBar) { }
 
@@ -21,17 +22,20 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(){
+    this.loading = true;
     if((this.credentials.username!='' && this.credentials.password!='') && (this.credentials.username!=null && this.credentials.password!=null)){
       this.loginservice.generateToken(this.credentials).subscribe(
         (response:any)=>{
           //success
           console.log(response);
+          this.loading = false;
           this.loginservice.loginUser(response.token);
           window.location.href = "/dashboard";
         },
         (error:any)=>{
           //error
           console.log(error);
+          this.loading = false;
           let snackBarRef = this.snackbar.open("Incorrect Credentials","Close");
         }
       );
